d9ba9b376c024451f9689ff37f9367b8b90f8b07
Jyoti Ranjan Biswal <jranjan2017@gmail.com>
2025-06-11 03:22:35 +0530

    merged

--- components/CostBenefit/Sidebar.js
+++ components/CostBenefit/Sidebar.js
@@ -2,6 +2,8 @@ import { RiArrowLeftSLine } from "react-icons/ri";
 import styles from './sidebar.module.css';
 import React from "react";
 import { useEffect } from "react";
+import Modal from "./Modal";
+import RoiReport from "./RoiReport";
 
 const formatCurrency = (value, currencyCode) => {
     if (!value || !currencyCode) return '0';
@@ -13,9 +15,15 @@ const formatCurrency = (value, currencyCode) => {
     }).format(value);
 };
 
-const Sidebar  = ({ handleSideBarView, showSideBar }) => {
+const Sidebar = ({ handleSideBarView, showSideBar, contentData }) => {
     const [spareParts, setSpareParts] = React.useState([]);
     const [sparePartData, setSparePartData] = React.useState(null);
+
+    const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
 
     const fetchSparePart = async () => {
         const response = await fetch('/api/sparepart');
@@ -53,104 +61,48 @@ const Sidebar  = ({ handleSideBarView, showSideBar }) => {
                                 <div className="flex flex-col gap-5">
                                     {/* Basic Info */}
                                     <div className="flex flex-col gap-2.5">
-                                        <p className="text-[#607797] text-base font-medium">Capacity of Line: {sparePartData?.clientMachineSparePart?.capacityOfLine?.value} {sparePartData?.clientMachineSparePart?.capacityOfLine?.unit}</p>
-                                        <p className="text-[#607797] text-base font-medium">Daily running Hours: {sparePartData?.clientMachineSparePart?.dailyRunningHours?.value} {sparePartData?.clientMachineSparePart?.dailyRunningHours?.unit}</p>
-                                        <p className="text-[#607797] text-base font-medium">Lifetime of rotor: {sparePartData?.clientMachineSparePart?.lifetimeOfRotor?.value} {sparePartData?.clientMachineSparePart?.lifetimeOfRotor?.unit}</p>
-                                        <p className="text-[#607797] text-base font-medium">Total running Hours: {sparePartData?.clientMachineSparePart?.totalRunningHours?.value} {sparePartData?.clientMachineSparePart?.totalRunningHours?.unit}</p>
+                                        <p className="text-[#607797] text-base font-medium">Capacity of Line: 400 tpd</p>
+                                        <p className="text-[#607797] text-base font-medium">Daily running Hours: 24 Hrs</p>
+                                        <p className="text-[#607797] text-base font-medium">Lifetime of rotor: 3600 Hrs</p>
+                                        <p className="text-[#607797] text-base font-medium">Total running Hours: 5040 Hrs</p>
                                     </div>
 
                                     {/* Fiber Loss Section */}
                                     <div className="flex flex-col gap-2.5">
                                         <h3 className="text-[#2d3e5c] text-lg font-bold">Fiber Loss</h3>
-                                        <p className="text-[#607797] text-base font-medium">Fiber Loss: {sparePartData?.clientMachineSparePart?.totalFiberLoss?.value} {sparePartData?.clientMachineSparePart?.totalFiberLoss?.unit}</p>
-                                        <p className="text-[#607797] text-base font-medium">Fiber Cost: {formatCurrency(
-                                            sparePartData?.clientMachineSparePart?.fiberCost?.value,
-                                            sparePartData?.clientMachineSparePart?.fiberCost?.priceUnit
-                                        )} / {sparePartData?.clientMachineSparePart?.fiberCost?.perUnit}</p>
-                                        <p className="text-[#607797] text-base font-medium">Total Fiber Loss Value: {formatCurrency(
-                                            sparePartData?.clientMachineSparePart?.totalFiberLossCost?.value,
-                                            sparePartData?.clientMachineSparePart?.totalFiberLossCost?.unit
-                                        )}</p>
+                                        <p className="text-[#607797] text-base font-medium">Fiber Loss: 92 tons</p>
+                                        <p className="text-[#607797] text-base font-medium">Fiber Cost: € 200/ton</p>
+                                        <p className="text-[#607797] text-base font-medium">Total Fiber Loss Value: € 18,400</p>
                                     </div>
 
                                     {/* Power Loss Section */}
                                     <div className="flex flex-col gap-2.5">
                                         <h3 className="text-[#2d3e5c] text-lg font-bold">Power Loss</h3>
-                                        <p className="text-[#607797] text-base font-medium">Installed Motor power: {sparePartData?.clientMachineSparePart?.installedMotorPower?.value} {sparePartData?.clientMachineSparePart?.installedMotorPower?.unit}</p>
-                                        <p className="text-[#607797] text-base font-medium">Power Cost: {formatCurrency(
-                                            sparePartData?.clientMachineSparePart?.powerCost?.value,
-                                            sparePartData?.clientMachineSparePart?.powerCost?.priceUnit
-                                        )} / {sparePartData?.clientMachineSparePart?.powerCost?.perUnit}</p>
-                                        <p className="text-[#607797] text-base font-medium">Total Power Cost: {formatCurrency(
-                                            sparePartData?.clientMachineSparePart?.totalPowerLossCost?.value,
-                                            sparePartData?.clientMachineSparePart?.totalPowerLossCost?.unit
-                                        )}</p>
+                                        <p className="text-[#607797] text-base font-medium">Installed Motor power: 500 kw</p>
+                                        <p className="text-[#607797] text-base font-medium">Power Cost: 0.09 €/kwhr</p>
+                                        <p className="text-[#607797] text-base font-medium">Total Power Cost: € 6,480</p>
                                     </div>
 
                                     {/* Total Loss Section */}
                                     <div className="flex flex-col gap-2.5">
                                         <h3 className="text-[#2d3e5c] text-lg font-bold">Total Loss</h3>
-                                        <p className="text-[#607797] text-base font-medium">Total Loss: {formatCurrency(
-                                            sparePartData?.clientMachineSparePart?.totalLossCost?.value,
-                                            sparePartData?.clientMachineSparePart?.totalLossCost?.unit
-                                        )}</p>
+                                        <p className="text-[#607797] text-base font-medium">Total Loss: € 24,880</p>
                                     </div>
                                 </div>
                             </div>
 
-                            {/* Content container */}
-                            <div className="flex flex-col gap-4 h-fit">
-                                {/* Key Data Points Section */}
-                                <div className="flex flex-col gap-4 p-4 h-[calc(100svh_-_350px)] overflow-y-auto [&::-webkit-scrollbar]:w-0 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:hidden [&::-webkit-scrollbar-thumb]:hidden [&::-webkit-scrollbar-thumb]:hiden dark:[&::-webkit-scrollbar-track]:bg-neutral-400 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
-                                    <h2 className="text-[#2d3e5c] text-xl font-medium font-montserrat">
-                                        Key Data Points
-                                    </h2>
+                            {/* Divider */}
+                            <div className="w-full h-[1px] bg-[#dfe6ec] my-4 absolute bottom-17"></div>
 
-                                    <div className="flex flex-col gap-5">
-                                        {/* Basic Info */}
-                                        <div className="flex flex-col gap-2.5">
-                                            <p className="text-[#607797] text-base font-medium">Capacity of Line: 400 tpd</p>
-                                            <p className="text-[#607797] text-base font-medium">Daily running Hours: 24 Hrs</p>
-                                            <p className="text-[#607797] text-base font-medium">Lifetime of rotor: 3600 Hrs</p>
-                                            <p className="text-[#607797] text-base font-medium">Total running Hours: 5040 Hrs</p>
-                                        </div>
-
-                                        {/* Fiber Loss Section */}
-                                        <div className="flex flex-col gap-2.5">
-                                            <h3 className="text-[#2d3e5c] text-lg font-bold">Fiber Loss</h3>
-                                            <p className="text-[#607797] text-base font-medium">Fiber Loss: 92 tons</p>
-                                            <p className="text-[#607797] text-base font-medium">Fiber Cost: € 200/ton</p>
-                                            <p className="text-[#607797] text-base font-medium">Total Fiber Loss Value: € 18,400</p>
-                                        </div>
-
-                                        {/* Power Loss Section */}
-                                        <div className="flex flex-col gap-2.5">
-                                            <h3 className="text-[#2d3e5c] text-lg font-bold">Power Loss</h3>
-                                            <p className="text-[#607797] text-base font-medium">Installed Motor power: 500 kw</p>
-                                            <p className="text-[#607797] text-base font-medium">Power Cost: 0.09 €/kwhr</p>
-                                            <p className="text-[#607797] text-base font-medium">Total Power Cost: € 6,480</p>
-                                        </div>
-
-                                        {/* Total Loss Section */}
-                                        <div className="flex flex-col gap-2.5">
-                                            <h3 className="text-[#2d3e5c] text-lg font-bold">Total Loss</h3>
-                                            <p className="text-[#607797] text-base font-medium">Total Loss: € 24,880</p>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                {/* Divider */}
-                                <div className="w-full h-[1px] bg-[#dfe6ec] my-4 absolute bottom-17"></div>
-
-                                {/* Detail Cost Report Button */}
-                                <div className="flex justify-centern absolute bottom-4 left-1/2 -translate-x-1/2 w-full">
-                                    <button onClick={() => setIsModalOpen(true)} className="cursor-pointer flex items-center gap-5 px-8 py-3 bg-[#f0f8fe] text-[#607797] rounded-full border-[1.5px] border-[#607797] hover:bg-[#e0f0fe] transition-colors mx-auto">
-                                        <span className="text-base font-semibold">Detail Cost Report</span>
-                                        {/* <Image src="/icon-dow.png" alt="Download" width={24} height={24} /> */}
-                                    </button>
-                                </div>
+                            {/* Detail Cost Report Button */}
+                            <div className="flex justify-centern absolute bottom-4 left-1/2 -translate-x-1/2 w-full">
+                                <button onClick={() => setIsModalOpen(true)} className="cursor-pointer flex items-center gap-5 px-8 py-3 bg-[#f0f8fe] text-[#607797] rounded-full border-[1.5px] border-[#607797] hover:bg-[#e0f0fe] transition-colors mx-auto">
+                                    <span className="text-base font-semibold">Detail Cost Report</span>
+                                    {/* <Image src="/icon-dow.png" alt="Download" width={24} height={24} /> */}
+                                </button>
                             </div>
                         </div>
+
                     </div>
 
                     {/* Icon Frame */}
